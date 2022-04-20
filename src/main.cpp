d468fb330dd0373d2a93866f85313c398fc21bed
Hong-Leon <hongjialiang@outlook.com>
2022-04-20 20:30:19 +0800

    episode2

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include "include/list.h"
+#include "include/database.h"
 
 int main(int arg_count, char *args[]){
+    List SimpleList;
+    Database data;
+
     if (arg_count > 1) {
-        List SimpleList;
         SimpleList.name = string(args[1]);
         SimpleList.print_menu();
+        data.write(SimpleList.list);
+        data.read();
     }
     else {
         cout << "Username not supplied .. exiting the program" << endl;
     }
+    
     return 0;
 }
